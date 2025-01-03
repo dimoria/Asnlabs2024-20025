2fb9fe2f20cc6c25a0734ba65cd8cdc10f61474a
ddub <di.dubenok05@gmail.com>
2025-01-03 19:18:42 +0200

    Doing task4 try to solve problem with txt file can't find it

--- src/main/java/tasks/Task4.java
+++ src/main/java/tasks/Task4.java
@@ -1,4 +1,26 @@
 package tasks;
+import java.io.*;
+import java.nio.file.*;
+import java.util.stream.*;
 
 public class Task4 {
+
+
+    public static void processLargeFile(String filePath) {
+        try (Stream<String> lines = Files.lines(Paths.get(filePath))) {
+
+            lines.filter(line -> line.contains("target"))
+                    .forEach(line -> {
+
+                        System.out.println("Processing: " + line);
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String filePath = "Smth.txt";
+        processLargeFile(filePath);
+    }
 }
