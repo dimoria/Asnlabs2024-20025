e4c1fd31c40a342d228ba1eb35af766e871843f1
ddub <di.dubenok05@gmail.com>
2025-01-03 15:24:41 +0200

    Task1 ready for check

---
+++ src/main/java/org/example/Task1.java
@@ -0,0 +1,23 @@
+package org.example;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.stream.*;
+
+public class Task1 {
+    public static <T, R> void asyncMap(List<T> input, Function<T, R> mapper, Consumer<List<R>> callback) {
+        ExecutorService executor = Executors.newCachedThreadPool();
+        List<CompletableFuture<R>> futures = input.stream()
+                .map(item -> CompletableFuture.supplyAsync(() -> mapper.apply(item), executor))
+                .collect(Collectors.toList());
+
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+                .thenApply(v -> futures.stream().map(CompletableFuture::join).collect(Collectors.toList()))
+                .thenAccept(callback)
+                .whenComplete((r, e) -> executor.shutdown());
+    }
+
+    public static void main(String[] args) {
+        asyncMap(Arrays.asList(1, 2, 3), x -> x * 2, result -> System.out.println("Async Map Result: " + result));
+    }
+}
