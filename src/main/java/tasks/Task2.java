dbc3de1548db88a14c859eb109ed89161bf3dce3
ddub <di.dubenok05@gmail.com>
2025-01-03 15:52:21 +0200

    Task2 start

--- src/main/java/tasks/Task2.java
+++ src/main/java/tasks/Task2.java
@@ -1,4 +1,23 @@
 package tasks;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.stream.*;
 
 public class Task2 {
+    public static <T, R> CompletableFuture<List<R>> promiseMap(List<T> input, Function<T, R> mapper) {
+        ExecutorService executor = Executors.newCachedThreadPool();
+        List<CompletableFuture<R>> futures = input.stream()
+                .map(item -> CompletableFuture.supplyAsync(() -> mapper.apply(item), executor))
+                .collect(Collectors.toList());
+
+        return CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+                .thenApply(v -> futures.stream().map(CompletableFuture::join).collect(Collectors.toList()))
+                .whenComplete((r, e) -> executor.shutdown());
+    }
+
+    public static void main(String[] args) {
+        promiseMap(Arrays.asList(4, 5, 6), x -> x * 3)
+                .thenAccept(result -> System.out.println("Promise Result: " + result));
+    }
 }
