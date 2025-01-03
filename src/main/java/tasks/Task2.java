9503b295b1dfabc826c1c5e4c58726451d084e20
ddub <di.dubenok05@gmail.com>
2025-01-03 15:56:13 +0200

    Refactor done, added async-await of Java version and added support of parallelism using ExecutorService

--- src/main/java/tasks/Task2.java
+++ src/main/java/tasks/Task2.java
@@ -19,5 +19,35 @@ public class Task2 {
     public static void main(String[] args) {
         promiseMap(Arrays.asList(4, 5, 6), x -> x * 3)
                 .thenAccept(result -> System.out.println("Promise Result: " + result));
+
+
+        CompletableFuture<Void> asyncAwaitExample = promiseMap(Arrays.asList(1, 2, 3), x -> x + 10)
+                .thenCompose(result -> CompletableFuture.supplyAsync(() -> {
+                    result.forEach(System.out::println);
+                    return null;
+                }));
+
+        asyncAwaitExample.join();
+
+
+        ExecutorService parallelExecutor = Executors.newWorkStealingPool();
+        List<CompletableFuture<Integer>> parallelFutures = Arrays.asList(1, 2, 3, 4, 5)
+                .stream()
+                .map(num -> CompletableFuture.supplyAsync(() -> {
+                    try {
+                        Thread.sleep(500); // Simulate work
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                    return num * 2;
+                }, parallelExecutor))
+                .collect(Collectors.toList());
+
+        CompletableFuture.allOf(parallelFutures.toArray(new CompletableFuture[0]))
+                .thenAccept(v -> parallelFutures.stream()
+                        .map(CompletableFuture::join)
+                        .forEach(result -> System.out.println("Parallel Result: " + result)));
+
+        parallelExecutor.shutdown();
     }
 }
