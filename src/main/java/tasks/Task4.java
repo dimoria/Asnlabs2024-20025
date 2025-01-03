00b45b7bba37295010d4dfa4e2b0852ddde53ba1
ddub <di.dubenok05@gmail.com>
2025-01-03 19:30:45 +0200

    Solved problem connected to finding txt file, developed code

--- src/main/java/tasks/Task4.java
+++ src/main/java/tasks/Task4.java
@@ -5,9 +5,17 @@ import java.util.stream.*;
 
 public class Task4 {
 
-
     public static void processLargeFile(String filePath) {
-        try (Stream<String> lines = Files.lines(Paths.get(filePath))) {
+        Path path = Paths.get(filePath);
+
+
+        if (!Files.exists(path)) {
+            System.out.println("File not found: " + filePath);
+            System.out.println("Absolute path: " + path.toAbsolutePath());  // Додаємо абсолютний шлях для перевірки
+            return;
+        }
+
+        try (Stream<String> lines = Files.lines(path)) {
 
             lines.filter(line -> line.contains("target"))
                     .forEach(line -> {
@@ -15,11 +23,14 @@ public class Task4 {
                         System.out.println("Processing: " + line);
                     });
         } catch (IOException e) {
+
+            System.err.println("Error reading the file: " + e.getMessage());
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
+
         String filePath = "Smth.txt";
         processLargeFile(filePath);
     }
